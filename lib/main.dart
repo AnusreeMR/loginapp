2f2c8acaf9d2eaf6138efd84e42071bea7db5a04
AnusreeMR <anus87647@gmail.com>
2025-01-03 15:33:48 +0530

    float button,

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/add_note_screen.dart';
 import 'package:flutter_application_1/firebase_options.dart';
+import 'package:flutter_application_1/login.dart';
 import 'package:flutter_application_1/registration.dart';
 
 Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
 );
-  runApp(MaterialApp(home: Registrationpage(),));
+  runApp(MaterialApp(home: LoginPage(),));
 }
